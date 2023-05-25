ba55df6318730b31a201ef72c1adf0c61cbd2462
paresiqbal <thecrowdel@gmail.com>
2023-05-25 12:48:08 +0700

    Tying uisng API

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,7 +1,27 @@
-export default function Post() {
+const getPostData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+const getUserData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
+export default async function Post() {
+  const [post, users] = await Promise.all([getPostData(), getUserData()]);
+
   return (
     <div>
       <h1>List of Post</h1>
+      {post.map((post: any) => {
+        return <p> {post.title}</p>;
+      })}
+      <div>
+        {users.map((user: any) => {
+          return <p>{user.name}</p>;
+        })}
+      </div>
     </div>
   );
 }
