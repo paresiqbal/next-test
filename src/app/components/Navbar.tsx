247a278d93faf3db1467f9e5c4c4873606276ae2
paresiqbal <thecrowdel@gmail.com>
2023-05-25 12:40:56 +0700

    Creating post

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="p-5 flex gap-10">
       <Link href={"/"}>Home</Link>
       <Link href={"/views/about"}>About</Link>
+      <Link href={"/post"}>Post</Link>
     </div>
   );
 }
